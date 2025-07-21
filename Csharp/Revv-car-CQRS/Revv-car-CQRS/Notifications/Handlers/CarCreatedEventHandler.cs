3c9cd40f796feae1bb5442260444a239a2e0e8dd
Rosemary <rosemarythomas994@gmail.com>
2025-07-21 13:46:28 +0530

    logger added

--- Csharp/Revv-car-CQRS/Revv-car-CQRS/Notifications/Handlers/CarCreatedEventHandler.cs
+++ Csharp/Revv-car-CQRS/Revv-car-CQRS/Notifications/Handlers/CarCreatedEventHandler.cs
@@ -7,9 +7,21 @@ namespace Revv_car_CQRS.Notifications.Handlers
 {
     public class CarCreatedEventHandler : INotificationHandler<CarCreatedEvent>
     {
+        private readonly ILogger<CarCreatedEventHandler> _logger;
+        public CarCreatedEventHandler(ILogger<CarCreatedEventHandler> logger)
+        {
+            _logger = logger;
+        }
         public Task Handle(CarCreatedEvent notification, CancellationToken cancellationToken)
         {
-            // You could log to file, DB, or call external API here
+            _logger.LogInformation("🚘 [EVENT FIRED] New car created:");
+            _logger.LogInformation("Car ID: {CarId}, Brand: {Brand}, Model: {Model}, Year: {Year}, Place: {Place}, UserId: {UserId}",
+                notification.CarId,
+                notification.Brand,
+                notification.Model,
+                notification.Year,
+                notification.Place,
+                notification.UserId);
             Console.WriteLine("🚘 [EVENT FIRED] A new car has been created:");
             Console.WriteLine($"   ID     : {notification.CarId}");
             Console.WriteLine($"   Brand  : {notification.Brand}");
